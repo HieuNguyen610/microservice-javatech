2f3ff2f61364f4779c7d30b244093fbf09e002a5
hieunm2 <harrynguyen610@gmail.com>
2025-04-06 20:39:15 +0700

    init

---
+++ src/main/java/com/mods/microservicejavatech/MicroserviceJavatechApplication.java
@@ -0,0 +1,13 @@
+package com.mods.microservicejavatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceJavatechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceJavatechApplication.class, args);
+    }
+
+}
